603dcfdd36557be6a51469ec1ecd2aa96f4b9d02
Your Name <diana.lopez.ceron@correounivalle.edu.co>
2026-02-23 21:49:56 -0500

    generacion de test basicos

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -5,11 +5,12 @@ import pytest
 
 @pytest.fixture
 def datos_banco():
+    """Fixture para cargar los datos del banco desde un archivo csv"""
     df = pd.read_csv("data/raw/bank-additional-full.csv", sep=";")
     return df
 
 def test_esquema(datos_banco):
-
+    """ Test de esquema para el Dataframe de datos_banco"""
     esquema = DataFrameSchema({
         "age": Column(int, nullable=False),
         "job": Column(str, nullable=False),
@@ -36,3 +37,37 @@ def test_esquema(datos_banco):
 
     esquema.validate(datos_banco)
 
+def test_basico(datos_banco):
+    """
+    Test básico para verificar que el DataFrame de datos_banco no está vacío
+    y contiene las columnas esperadas.
+
+    Args:
+        datos_banco (pd.DataFrame): DataFrame que contiene los datos del banco.
+    """
+    df = datos_banco
+
+    # Verificar que el DataFrame no está vacío
+    assert not df.empty, "El DataFrame está vacío."
+    # Verificar nulos
+    assert df.isnull().sum().sum() == 0, "El DataFrame contiene valores nulos."
+    # Verificar duplicados
+    # assert df.duplicated().sum() == 0, "El DataFrame contiene filas duplicadas."
+    # Verificar cantidad de columnas
+    assert df.shape[1] == 21, f"El DataFrame debería tener 21 columnas, pero tiene {df.shape[1]}."
+
+
+if __name__ == "__main__":
+    try:
+        test_esquema(datos_banco())
+        test_basico(datos_banco())
+        print("Todos los tests pasaron exitosamente.")
+
+        with open("docs/test_results/test_results.txt", "w") as f:
+            f.write("Todos los tests pasaron exitosamente.\n")
+
+    except AssertionError as e:
+        print(f"Test fallido: {e}")
+
+        with open("docs/test_results/test_results.txt", "w") as f:
+            f.write(f"Test fallido: {e}\n")
